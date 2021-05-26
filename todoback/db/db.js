898ebc4cbd7648019483639076eda02332e1c6ac
himanshuBook <mailbyhimanshu@gmail.com>
2021-05-26 13:03:29 +0530

    all

--- todoback/db/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-try {
-  mongoose.connect(process.env.DB);
-  console.log("Connected to database");
-} catch {
-  console.log("Error connecting to database");
-}
